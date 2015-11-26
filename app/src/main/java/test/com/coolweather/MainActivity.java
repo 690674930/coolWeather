5d405ae7f9f41d6d99295cc49a5a5c0cb9acadfc
690674930 <690674930@qq.com>
2015-11-26 20:50:16 +0800

    first commit

---
+++ app/src/main/java/test/com/coolweather/MainActivity.java
@@ -0,0 +1,15 @@
+package test.com.coolweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
